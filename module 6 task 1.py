020844384c29bcaa56abf8d522530d0cac6dee6d
AJL <alissa.li@edubga.com>
2026-02-09 16:50:44 +0000

    red TDD test to check for valid format

--- module 6 task 1.py
+++ module 6 task 1.py
@@ -20,6 +20,31 @@ from ftplib import FTP
 
 def open_patient_file_and_save_data(patient):
   patient1 = open(patient, "r")
-  return print(patient1.readable()), print(patient1.read())
-
+  return print(patient1.readable()), print(patient1.read()), patient1
+   
 open_patient_file_and_save_data("patient files/valid/MED_DATA_20230603140104.csv")
+
+def test_for_valid_format(patient):
+    patient2 = patient 
+    str, numb ("seperates numbers and letters")
+    century = numb(0:3)
+    year = numb(3:5)
+    month = numb(5:7)
+    day = numb
+    hour = numb
+    minute = numb
+    second = numb
+    if century >= 21:
+     return False
+    if year >= 27:
+      return False
+    if month >= 13:
+      return False
+    if day >= 32:
+      return False
+    if hour >= 25:
+      return False
+    if minute > 61:
+      return False
+    if month > 61:
+      return False
