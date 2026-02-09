2a594ce7918fe88153873f10bbe81270541677f9
AJL <alissa.li@edubga.com>
2026-02-09 21:48:56 +0000

    red TDD fail test for batch ID duplication checking unit

--- module 6 task 1.py
+++ module 6 task 1.py
@@ -1,29 +1,36 @@
 
 from ftplib import FTP
 
-""" def test_open_remote_FTP_server():
+""" 
+def test_open_remote_FTP_server():
     ftp.connect("server url")
-    """
+"""
 #---------------------------------------------------------------------------    
 # moving onto file opening function as the mock ftp server is not built yet
 #---------------------------------------------------------------------------
-""" def test_show_all_dict_names():
-    ftp.nlst("lists all files in the ftp directory")
-    """
+""" 
+def test_show_all_dict_names():
+ftp.nlst("lists all files in the ftp directory")
+"""
 #------------------------------------------------------
 # moving on because ftp server is not set up yet
 #------------------------------------------------------
 
 #-----------------------------------------------------------------------------------------------------
-# i will be using a mock file created from appedix example as the file generation has not been set up
+""" 
+open file unit
+i will be using a mock file created from appedix example as the file generation has not been set up
+"""
 #-----------------------------------------------------------------------------------------------------
-
 def open_patient_file_and_save_data(patient):
   patient1 = open(patient, "r")
   return print(patient1.readable()), print(patient1.read()), patient1
    
 open_patient_file_and_save_data("patient files/valid/MED_DATA_20230603140104.csv")
 
+#---------------------------------------------
+# validation check for correct date format
+#---------------------------------------------
 def check_for_valid_format(patient):
     numb = ""
     for ch in patient:
@@ -60,3 +67,25 @@ def check_for_valid_format(patient):
     else:
       return print(True) 
 patient = check_for_valid_format("patient files/valid/MED_DATA_20230603140104.csv")
+
+#----------------------------------------
+# duplication check unit
+#----------------------------------------
+import csv 
+from collections import Counter
+
+def test_for_dup_batch_ID(patient):
+    column_name = ("isolated column")
+    extracted_batch_ID = []
+    open("patientfile") as valuepair:
+    patient_data = read("patientfile")
+    for inter_over_row in patient_data:
+        extracted_batch_ID.append({def col: def row, creating valuepairs})
+        batch_ID = extracted_batch_ID.get("isolated column")
+    batch_ID_dic = Counter(batch_ID)
+    batch_ID_final_test = list(batch_ID_dic.value)) 
+    for ID in batch_ID_final_test:
+        if ID > 1:
+        return False
+    else:
+        return True
