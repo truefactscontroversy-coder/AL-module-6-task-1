a9a9221b374a1352a63aa7964a9262bff58983f4
AJL <alissa.li@edubga.com>
2026-02-09 15:21:10 +0000

    red TDD test for open file unit

--- module 6 task 1.py
+++ module 6 task 1.py
@@ -1,7 +1,24 @@
 
 from ftplib import FTP
 
-def open_remote_FTP_server():
+""" def test_open_remote_FTP_server():
     ftp.connect("server url")
-    
+    """
+#---------------------------------------------------------------------------    
+# moving onto file opening function as the mock ftp server is not built yet
+#---------------------------------------------------------------------------
+""" def test_show_all_dict_names():
+    ftp.nlst("lists all files in the ftp directory")
+    """
+#------------------------------------------------------
+# moving on because ftp server is not set up yet
+#------------------------------------------------------
 
+#-----------------------------------------------------------------------------------------------------
+# i will be using a mock file created from appedix example as the file generation has not been set up
+#-----------------------------------------------------------------------------------------------------
+def test_open_patient_file_and_save_data():
+  patient = open("patient file name", "r")
+  patientfile.readable
+
+test_open_patient_file_and_save_data()
