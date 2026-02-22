b57b0eb9a1b0f48bff1e1620544f15052404606d
AJL <alissa.li@edubga.com>
2026-02-22 16:07:49 +0000

    green TDD test for auto log unit

--- logging and automation/api error log tool.py
+++ logging and automation/api error log tool.py
@@ -3,20 +3,23 @@ import logging
 import requests
 
 
-
+def add(a, b):
+    a += b
+    return a
 
 
 def automated_logging(log_message):
-    api_url = "uuid generator"
+    api_url = "https://www.uuidtools.com/api/generate/v1"
 
     UUID_code = requests.get(api_url)
     uuid_code = str(UUID_code.json())
 
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
+
 
     formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(module)s:%(custom_attribute)s:%(message)s")
-    file_handler = logging.FileHandler("target file")
+    file_handler = logging.FileHandler("file log.txt", mode="a")
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
     file_handler.setFormatter(formatter)
 
     old_factory = logging.getLogRecordFactory()
@@ -29,13 +32,11 @@ def automated_logging(log_message):
     logging.setLogRecordFactory(record_factory)
 
 
-    uuid_logger = logging.FileHandler(uuid_code)
 
-    logger.addHandler(uuid_logger)
 
     logger.addHandler(file_handler)
 
 
     return logger.info(log_message)
  
-automated_logging(test_function)
+automated_logging(add(5,5))
