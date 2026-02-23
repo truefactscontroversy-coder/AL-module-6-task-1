d19dd097dd483255f289b342abd174f26a7ee26a
AJL <alissa.li@edubga.com>
2026-02-23 13:08:09 +0000

    red TDD test for random filename generator unit

--- logging and automation/auto_log_and_file_generator.py
+++ logging and automation/auto_log_and_file_generator.py
@@ -3,7 +3,7 @@ import requests
 import random
 import os
 import csv
-
+from datetime import datetime
 
 def auto_log(log_message):
     api_url = "https://www.uuidtools.com/api/generate/v1"
@@ -53,7 +53,19 @@ def random_headers():
     
     return shuffled_headers 
 
+def correct_filename_generator():
+    patient_data_name = ""
+    start_date = "20230603140104"
+    end_date = "20261231235959"
+    start_date = datetime.strptime(start_date, "chosen format")
+    end_date = datetime.strptime(end_date, "chosen format")
+    random_date = start_date + (end_date - start_date) * random.random()
+    random_date = random_date.strftime("chosen format")
+    patient_data_name = "MED_DATA_" + str(random_date)
+    return patient_data_name
 
+
+auto_log(correct_filename_generator())
 
 def correct_file_generator():
     file_data = ["batch_id", "timestamp", "reading1", "reading2", "reading3", "reading4", "reading5", "reading6", "reading7", "reading8", "reading9", "reading10"]
